3009c1a45d06d1cfffb5b94b08a0142704c714c8
Pham Tu <tubackkhoa@gmail.com>
2023-05-19 14:30:50 +0700

    update sdk

--- packages/contracts-build/src/index.ts
+++ packages/contracts-build/src/index.ts
@@ -15,26 +15,16 @@ export type InstantiateMsg =
   | OraiswapRouterTypes.InstantiateMsg
   | OraiswapStakingTypes.InstantiateMsg;
 
-export type MigrateMsg = OraiswapStakingTypes.MigrateMsg;
-
 const contractDir = path.join(path.dirname(module.filename), '..', 'data');
 
 export const getContractDir = (name: ContractName = 'oraiswap_limit_order') => {
   return path.join(contractDir, name + '.wasm');
 };
 
-export const deployContract = async (client: SigningCosmWasmClient, senderAddress: string, msg: InstantiateMsg, label: string, contractName?: ContractName) => {
+export const deployContract = async (client: SigningCosmWasmClient, senderAddress: string, contractName?: ContractName, msg?: InstantiateMsg, label?: string) => {
   // upload and instantiate the contract
   const wasmBytecode = readFileSync(getContractDir(contractName));
   const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
-  const initRes = await client.instantiate(senderAddress, uploadRes.codeId, msg, label, 'auto');
+  const initRes = await client.instantiate(senderAddress, uploadRes.codeId, msg ?? {}, label ?? contractName, 'auto');
   return { ...uploadRes, ...initRes };
-};
-
-export const migrateContract = async (client: SigningCosmWasmClient, senderAddress: string, contractAddress: string, msg: MigrateMsg, contractName?: ContractName) => {
-  // upload and instantiate the contract
-  const wasmBytecode = readFileSync(getContractDir(contractName));
-  const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
-  const migrateRes = await client.migrate(senderAddress, contractAddress, uploadRes.codeId, msg, 'auto');
-  return { ...uploadRes, ...migrateRes };
 };
