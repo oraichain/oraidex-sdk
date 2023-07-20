c67c71e541ec7c7b7cbe8a3f51085c88027a83a4
ducphamle2 <ducphamle212@gmail.com>
2023-07-20 12:45:25 +0700

    reverted 24h volume

--- packages/oraidex-server/src/helper.ts
+++ packages/oraidex-server/src/helper.ts
@@ -2,6 +2,8 @@ export function parseSymbolsToTickerId(symbols: [string, string]) {
   return `${symbols[0]}_${symbols[1]}`;
 }
 
-export function getDate24hBeforeNow() {
-  return new Date(new Date().setUTCHours(0, 0, 0, 0));
+export function getDate24hBeforeNow(time: Date) {
+  const twentyFourHoursInMilliseconds = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+  const date24hBeforeNow = new Date(time.getTime() - twentyFourHoursInMilliseconds);
+  return date24hBeforeNow;
 }
