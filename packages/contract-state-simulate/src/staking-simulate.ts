ef3b74115f75fa6abdda0ffb31c8d1a22a6af80c
Pham Tu <tubackkhoa@gmail.com>
2023-11-17 17:15:52 +0700

    fix testing

--- packages/contract-state-simulate/src/staking-simulate.ts
+++ packages/contract-state-simulate/src/staking-simulate.ts
@@ -1,6 +1,6 @@
 import { DownloadState } from "@oraichain/cw-simulate";
 import { SimulateCosmWasmClient } from "@oraichain/cw-simulate";
-import { AIRI_CONTRACT, STAKING_CONTRACT } from "@oraichain/oraidex-common";
+import { MILKY_CONTRACT, STAKING_CONTRACT } from "@oraichain/oraidex-common";
 import path from "path";
 import { readFileSync } from "fs";
 
@@ -11,10 +11,10 @@ const download = new DownloadState("https://lcd.orai.io", path.join(__dirname, "
 const senderAddress = "orai1gkr56hlnx9vc7vncln2dkd896zfsqjn300kfq0";
 const client = new SimulateCosmWasmClient({ chainId: "Oraichain", bech32Prefix: "orai" });
 await download.loadState(client, senderAddress, STAKING_CONTRACT, "mainnet staking contract");
-const pools = await client.queryContractSmart(STAKING_CONTRACT, {
-  pool_info: { asset_info: { token: { contract_addr: AIRI_CONTRACT } } }
+const pool = await client.queryContractSmart(STAKING_CONTRACT, {
+  pool_info: { asset_info: { token: { contract_addr: MILKY_CONTRACT } } }
 });
-console.log("pool: ", pools);
+console.log("pool: ", pool);
 
 // cwtools build ../oraiswap/contracts/oraiswap_staking -o packages/contract-state-simulate/src/wasm
 const { codeId } = await client.upload(
@@ -26,11 +26,8 @@ const { codeId } = await client.upload(
 const migrateResult = await client.migrate(senderAddress, STAKING_CONTRACT, codeId, {}, "auto");
 console.log("migrate result: ", migrateResult);
 
-// const newStakingContract = new OraiswapStakingClient(client, senderAddress, STAKING_CONTRACT);
-// const pool = await newStakingContract.poolInfo({ stakingToken: pools.staking_token });
-// console.log("pool: ", pool)
+const newPools = await client.queryContractSmart(STAKING_CONTRACT, {
+  pool_info: { staking_token: pool.staking_token }
+});
 
-// new key after migrate should be lp token instead of airi
-
-// console.log(await contract.topHolders({ limit: 30 }));
-// })();
+console.log("pool: ", newPools);
