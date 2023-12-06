4ddb1d5fed277b3ad19a0c5d680b9a6b06da9c5f
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-12-06 16:47:05 +0700

    fix symbols wTRX

--- packages/oraidex-sync/tests/helper.spec.ts
+++ packages/oraidex-sync/tests/helper.spec.ts
@@ -215,7 +215,7 @@ describe("test-helper", () => {
       {
         asset_infos: [{ native_token: { denom: ORAI } }, { token: { contract_addr: tronCw20Address } }],
         lp_token: pairLpTokens.TRX_ORAI,
-        symbols: ["ORAI", "WTRX"]
+        symbols: ["ORAI", "wTRX"]
       },
       {
         asset_infos: [{ token: { contract_addr: scAtomCw20Address } }, { native_token: { denom: atomIbcDenom } }],
