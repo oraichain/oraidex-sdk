66922130ad9a4b218cfd2b11b1df161cf3c21794
Hieu Le <levanhieuhust@gmail.com>
2023-12-14 10:09:52 +0700

    add fetchRetry function

--- packages/oraidex-common/src/helper.ts
+++ packages/oraidex-common/src/helper.ts
@@ -402,3 +402,25 @@ export const toSumDisplay = (amounts: AmountDetails): number => {
   }
   return amount;
 };
+
+export type RetryOptions = {
+  retry?: number;
+  timeout?: number;
+  callback?: (retry: number) => void;
+};
+
+const fetchRetry = async (url: RequestInfo | URL, opts: RequestInit & RetryOptions = {}) => {
+  let { retry = 3, callback, timeout = 30000, ...init } = opts;
+  init.signal = AbortSignal.timeout(timeout);
+  while (retry > 0) {
+    try {
+      return await fetch(url, init);
+    } catch (e) {
+      callback?.(retry);
+      retry--;
+      if (retry === 0) {
+        throw e;
+      }
+    }
+  }
+};
