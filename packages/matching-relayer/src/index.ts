b590cafc55e603b04071c9cbc1a5658d87424da5
Hieu Le <levanhieuhust@gmail.com>
2024-01-02 17:26:20 +0700

    comment log

--- packages/matching-relayer/src/index.ts
+++ packages/matching-relayer/src/index.ts
@@ -36,7 +36,7 @@ export async function matchingOrders(
   const orderbook = new OraiswapLimitOrderQueryClient(sender.client, contractAddr);
   const query_pairs = await orderbook.orderBooks({});
 
-  console.log(`Excecuting orderbook contract ${contractAddr}`);
+  // console.log(`Excecuting orderbook contract ${contractAddr}`);
 
   let execute_pairs: any[] = [];
   for (let pair in query_pairs.order_books) {
@@ -61,7 +61,7 @@ export async function matchingOrders(
   }
 
   const { amount } = await sender.client.getBalance(sender.address, denom);
-  console.log(`balance of ${sender.address} is ${amount}`);
+  // console.log(`balance of ${sender.address} is ${amount}`);
   if (parseInt(amount) <= minimumOraiBalance) {
     throw new Error(`Balance(${amount}) of ${sender.address} must be greater than 1 ORAI`);
   }
