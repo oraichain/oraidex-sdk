b365fcba8f48fe5025de102223692ab3dc8c589d
trung2891 <trungnv280201@gmail.com>
2024-03-15 15:18:13 +0700

    chore: gent proto normally

--- packages/universal-swap/src/proto/proto-gen.ts
+++ packages/universal-swap/src/proto/proto-gen.ts
@@ -1,16 +1,35 @@
-import { IbcBridgeWasmMemo, IbcHooksMemo } from "./universal-swap_pb";
 import { decode, fromWords } from "bech32";
+import protobuf from "protobufjs";
+
 export const parseToIbcWasmMemo = (
   destinationReceiver: string,
   destinationChannel: string,
   destinationDenom: string
 ): string => {
-  const wasmMemo = new IbcBridgeWasmMemo();
-  wasmMemo.setDestinationReceiver(destinationReceiver);
-  wasmMemo.setDestinationChannel(destinationChannel);
-  wasmMemo.setDestinationDenom(destinationDenom);
+  const protobufRoot = protobuf.Root.fromJSON({
+    nested: {
+      IbcBridgeWasmMemo: {
+        fields: {
+          destinationReceiver: {
+            type: "string",
+            id: 1
+          },
+          destinationChannel: {
+            type: "string",
+            id: 2
+          },
+          destinationDenom: {
+            type: "string",
+            id: 3
+          }
+        }
+      }
+    }
+  });
 
-  return Buffer.from(wasmMemo.serializeBinary()).toString("base64");
+  const msg = protobufRoot.lookupType("IbcBridgeWasmMemo");
+  const message = msg.create({ destinationReceiver, destinationChannel, destinationDenom });
+  return Buffer.from(msg.encode(message).finish()).toString("base64");
 };
 
 export const parseToIbcHookMemo = (
@@ -19,11 +38,38 @@ export const parseToIbcHookMemo = (
   destinationChannel: string,
   destinationDenom: string
 ): string => {
-  const wasmMemo = new IbcHooksMemo();
-  // we will encode receiver from Orai address to canonicalize address
-  if (receiver) wasmMemo.setReceiver(Buffer.from(fromWords(decode(receiver).words)));
-  wasmMemo.setDestinationReceiver(destinationReceiver);
-  wasmMemo.setDestinationChannel(destinationChannel);
-  wasmMemo.setDestinationDenom(destinationDenom);
-  return Buffer.from(wasmMemo.serializeBinary()).toString("base64");
+  const protobufRoot = protobuf.Root.fromJSON({
+    nested: {
+      IbcHooksMemo: {
+        fields: {
+          receiver: {
+            type: "bytes",
+            id: 1
+          },
+          destinationReceiver: {
+            type: "string",
+            id: 2
+          },
+          destinationChannel: {
+            type: "string",
+            id: 3
+          },
+          destinationDenom: {
+            type: "string",
+            id: 4
+          }
+        }
+      }
+    }
+  });
+
+  const receiverCanonical = receiver ? Buffer.from(fromWords(decode(receiver).words)) : "";
+  const msg = protobufRoot.lookupType("IbcHooksMemo");
+  const message = msg.create({
+    receiver: receiverCanonical,
+    destinationReceiver,
+    destinationChannel,
+    destinationDenom
+  });
+  return Buffer.from(msg.encode(message).finish()).toString("base64");
 };
