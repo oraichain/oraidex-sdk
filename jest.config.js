1a47d45cf2db2e021d9c0f1878baf9c34aba4cb5
ducphamle2 <ducphamle212@gmail.com>
2024-03-19 12:42:05 -0700

    feat: add code coverage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,14 @@ module.exports = {
     "^.+\\.ts?$": ["ts-jest", { isolatedModules: true }]
   },
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/packages/ibc-routing"]
+  modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/packages/ibc-routing"],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "<rootDir>/packages/contracts-sdk",
+    "<rootDir>/packages/contracts-build",
+    "<rootDir>/packages/.+/build", // ignore every build/ of every sub directory of packages
+    "<rootDir>/packages/.+/dist",
+    "<rootDir>/node_modules/",
+    "<rootDir>/packages/.+/node_modules/"
+  ]
 };
