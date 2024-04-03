1ea0f6f1d0a5c867da2a88bfb507365255e80963
perfogic <longdieu12x@gmail.com>
2024-04-04 01:14:58 +0700

    chore: add data evm-to-evm for testing

---
+++ packages/ibc-routing/test/sample.spec.ts
@@ -0,0 +1,34 @@
+import { Gravity, Gravity__factory } from "@oraichain/oraidex-common";
+import { ethers } from "ethers";
+
+describe.skip("Test sync ether", () => {
+  it("Listen to event", async () => {
+    const provider = new ethers.providers.JsonRpcProvider("https://go.getblock.io/5364b225d0ea429e91f5f3f027c414a2");
+    const gravity: Gravity = Gravity__factory.connect(
+      ethers.utils.getAddress("0xb40C364e70bbD98E8aaab707A41a52A2eAF5733f"),
+      provider
+    );
+    const eventFilter = gravity.filters["SendToCosmosEvent(address,address,string,uint256,uint256)"](
+      "0x55d398326f99059fF775485246999027B3197955",
+      "0x9a0A02B296240D2620E339cCDE386Ff612f07Be5"
+    );
+    const data = await gravity.queryFilter(eventFilter, 37534323, 37534325);
+    console.log(data, data[0].args);
+  }, 30000);
+});
+
+describe("Test sync ether", () => {
+  it("Listen to event", async () => {
+    const provider = new ethers.providers.JsonRpcProvider("https://go.getblock.io/0efb9bd03a704cc8ad6cad84999bed4f");
+    const gravity: Gravity = Gravity__factory.connect(
+      ethers.utils.getAddress("0x09Beeedf51AA45718F46837C94712d89B157a9D3"),
+      provider
+    );
+    const eventFilter = gravity.filters["TransactionBatchExecutedEvent(uint256,address,uint256)"](
+      "4127",
+      "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+    );
+    const data = await gravity.queryFilter(eventFilter, 19574735, 19574737);
+    console.log(data, data[0].args);
+  }, 30000);
+});
