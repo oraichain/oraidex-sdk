3bbd8195c67e9673708542ab2c332e3f936db988
ducphamle2 <ducphamle212@gmail.com>
2024-04-07 11:28:08 -0700

    chore: add event type OraichainHandler handleEvent

--- packages/ibc-routing/src/event-handlers/oraichain.handler.ts
+++ packages/ibc-routing/src/event-handlers/oraichain.handler.ts
@@ -1,10 +1,11 @@
+import { Event } from "@cosmjs/stargate";
 import { invokableMachineStateKeys } from "../constants";
 import { EventHandler } from "./event.handler";
 
 export class OraichainHandler extends EventHandler {
   public handleEvent(eventData: any[]) {
     for (const eventItem of eventData) {
-      const events = eventItem.result.events;
+      const events: Event[] = eventItem.result.events;
       // FIXME: we should not use events.find here. we need to exhaustively search for the attr type as one tx can include many transactions
       if (events.find((attr) => attr.type === "recv_packet")) {
         this.im.transitionInterpreters(invokableMachineStateKeys.STORE_ON_RECV_PACKET_ORAICHAIN, eventItem);
