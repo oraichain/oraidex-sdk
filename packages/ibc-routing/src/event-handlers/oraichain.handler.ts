e4964b832d787905bf9fb31ea0ca16f6abdce608
ducphamle2 <ducphamle212@gmail.com>
2024-04-07 11:27:47 -0700

    chore: refactor OraichainHandler

--- packages/ibc-routing/src/event-handlers/oraichain.handler.ts
+++ packages/ibc-routing/src/event-handlers/oraichain.handler.ts
@@ -6,13 +6,13 @@ export class OraichainHandler extends EventHandler {
     for (const eventItem of eventData) {
       const events = eventItem.result.events;
       // FIXME: we should not use events.find here. we need to exhaustively search for the attr type as one tx can include many transactions
-      switch (true) {
-        case events.find((attr) => attr.type === "recv_packet") !== undefined:
-          this.im.transitionInterpreters(invokableMachineStateKeys.STORE_ON_RECV_PACKET_ORAICHAIN, eventItem);
-          break;
-        case events.find((attr) => attr.type === "acknowledge_packet") !== undefined:
-          this.im.transitionInterpreters(invokableMachineStateKeys.STORE_ON_ACKNOWLEDGEMENT_ORAICHAIN, eventItem);
-          break;
+      if (events.find((attr) => attr.type === "recv_packet")) {
+        this.im.transitionInterpreters(invokableMachineStateKeys.STORE_ON_RECV_PACKET_ORAICHAIN, eventItem);
+        return;
+      }
+      if (events.find((attr) => attr.type === "acknowledge_packet")) {
+        this.im.transitionInterpreters(invokableMachineStateKeys.STORE_ON_ACKNOWLEDGEMENT_ORAICHAIN, eventItem);
+        return;
       }
     }
   }
