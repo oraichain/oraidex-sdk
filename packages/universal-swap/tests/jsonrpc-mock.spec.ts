1d39d68ec2044a321ceeabd39845b5c47f8dd6d8
ducphamle2 <ducphamle212@gmail.com>
2024-04-08 16:35:20 -0700

    chore: add test-mock-rpc-getAccount

--- packages/universal-swap/tests/jsonrpc-mock.spec.ts
+++ packages/universal-swap/tests/jsonrpc-mock.spec.ts
@@ -2,12 +2,18 @@ import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { GasPrice, SigningStargateClient } from "@cosmjs/stargate";
 import { ORAI, mockJsonRpcServer } from "@oraichain/oraidex-common";
 import "dotenv/config";
+import * as mockttp from "mockttp";
 // configuration
 
 describe("test-nock", () => {
+  let server: mockttp.Mockttp;
+  beforeEach(async () => {
+    server = await mockJsonRpcServer();
+  });
+  afterEach(async () => {
+    await server.stop();
+  });
   it("test-mock-rpc-post", async () => {
-    const server = await mockJsonRpcServer();
-
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(process.env.MNEMONIC as any, { prefix: ORAI });
     const accounts = await wallet.getAccounts();
     const stargate = await SigningStargateClient.connectWithSigner(server.url, wallet, {
@@ -19,9 +25,17 @@ describe("test-nock", () => {
       [{ amount: "1", denom: ORAI }],
       "auto"
     );
-    console.log({ result });
     expect(result.transactionHash).toEqual("2F59A5318ED6350976244EAD25CB78ACFEBC1B9C9A96809269A793EDFB529065");
     expect(result.height).toEqual(1);
-    await server.stop();
+  });
+
+  it("test-mock-rpc-getAccount", async () => {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(process.env.MNEMONIC as any, { prefix: ORAI });
+    const accounts = await wallet.getAccounts();
+    const stargate = await SigningStargateClient.connectWithSigner(server.url, wallet, {
+      gasPrice: GasPrice.fromString("0.001orai")
+    });
+    const result = await stargate.getAccount(accounts[0].address);
+    expect(result.address).toEqual("orai1g4h64yjt0fvzv5v2j8tyfnpe5kmnetejvfgs7g");
   });
 });
