86e970456823a98a2438df4921c58506ba2ee385
ducphamle2 <ducphamle212@gmail.com>
2024-04-09 16:12:33 -0700

    update testing stargate-client query

--- packages/ibc-routing/test/search-tx.spec.ts
+++ packages/ibc-routing/test/search-tx.spec.ts
@@ -1,22 +1,20 @@
 import { StargateClient } from "@cosmjs/stargate";
 import { QueryTag } from "@cosmjs/tendermint-rpc/build/tendermint37";
 import { buildQuery } from "@cosmjs/tendermint-rpc/build/tendermint37/requests";
-import { autoForwardTag } from "../src/constants";
 
 describe("Testing searchTx using rpc", () => {
   it("Try testing stargate-client query", async () => {
     const queryTags: QueryTag[] = [
-      autoForwardTag,
+      // autoForwardTag,
       {
-        key: "nonce",
-        value: "63759"
+        key: "gravity.v1.EventSendToCosmosExecutedIbcAutoForward.nonce",
+        value: "64275"
       }
     ];
     encodeURI;
     const query = buildQuery({
       tags: queryTags
     });
-    console.log(`${encodeURIComponent("gravity.v1.EventSendToCosmosExecutedIbcAutoForward")}.nonce='63759'`);
     const stargateClient = await StargateClient.connect("https://bridge-v2.rpc.orai.io");
     const txs = await stargateClient.searchTx(query);
     console.log(txs);
