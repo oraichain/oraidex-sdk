829e739aedce5f89b58d1f147ca3ee996a0f7255
perfogic <longdieu12x@gmail.com>
2024-05-03 17:24:59 +0700

    chore: add log for debug on server

--- packages/ibc-routing/test/search-tx.spec.ts
+++ packages/ibc-routing/test/search-tx.spec.ts
@@ -2,12 +2,13 @@ import { StargateClient } from "@cosmjs/stargate";
 import { QueryTag } from "@cosmjs/tendermint-rpc/build/tendermint37";
 import { buildQuery } from "@cosmjs/tendermint-rpc/build/tendermint37/requests";
 
-describe.skip("Testing searchTx using rpc", () => {
+describe("Testing searchTx using rpc", () => {
   it("Try testing stargate-client query", async () => {
     const queryTags: QueryTag[] = [
+      { key: "batched_tx_ids.batched_tx_id", value: "34116" },
       {
-        key: "gravity.v1.EventSendToCosmosExecutedIbcAutoForward.nonce",
-        value: "65220"
+        key: "gravity.v1.EventOutgoingBatch.bridge_contract",
+        value: "0xb40C364e70bbD98E8aaab707A41a52A2eAF5733f"
       }
     ];
     const query = buildQuery({
